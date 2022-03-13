7fb6efd4fc160a797f794346b8063655d32e47e7
SLOKnightfall <SLOKnightfall@gmail.com>
2022-03-13 12:01:34 -0700

    -Repositioned some additional frames to try to solve pixel issue

--- Plugins/TransmogOutfits.lua
+++ Plugins/TransmogOutfits.lua
@@ -93,6 +93,8 @@ end
 --Catches the TransmogOutfit frame and clears its OnEven script.  This keeps the addons's buttons from being shown.
 addon:SecureHook("transmogOutfitFrameCreate", function (frame) 
 	frame:SetParent(addon.prisonFrame)
+	frame:ClearAllPoints()
+	frame:SetPoint("TOPRIGHT",100, 100)
 	frame:SetSize(1, 1)
 	frame:SetScript("OnEvent", function () end )
 end)
